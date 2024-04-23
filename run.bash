7058fe1a85564cc5ba75099d5f256042245ed0d4
Cristiano <jcristiano.c@gmail.com>
2024-04-23 19:51:34 -0300

    Desenvolvimento

--- run.bash
+++ run.bash
@@ -2,11 +2,36 @@
 
 set -x
 
-export S_CONTAINER_NAME="senai-app-php"
-export S_IMAGE_NAME="senai-jcmsilv-conuv-php:0.0.1"
+S_CONTAINER_NAME="senai-app-php"
+S_IMAGE_NAME="senai-jcmsilv-conuv-php:0.0.1"
 
+function fn_container_is_running(){
+    local s_container_name=${1}
+    if [[ docker ps --format '{{.Names}}' | grep -q "^${s_container_name}\$" ]]; then
+        return 1
+    fi
+    return 0    
+}
 
-if $( docker ps -a --format '{{.Names}}' | grep -q "^${S_CONTAINER_NAME}\$" ); then    
+function fn_image_exists(){
+    local s_image_name=${1}
+    if [[ docker ps -a --format '{{.Names}}' | grep -q "^${s_image_name}\$" ]];
+        return 1
+    fi
+    return 0
+}
+
+function fn_main(){
+    if [[ fn_container_is_running ${S_CONTAINER_NAME} -eq 1 ]]; then
+        echo "Parando o container: ${S_CONTAINER_NAME}"
+        docker container stop "${S_CONTAINER_NAME}"
+    fi
+}
+
+fn_main
+exit 0
+if $( docker ps -a --format '{{.Names}}' | grep -q "^${S_CONTAINER_NAME}\$" ); then
+    docker container stop "${}"    
     docker rm -f "${S_CONTAINER_NAME}"
     echo "Container removido: ${S_CONTAINER_NAME}"
 else
